45bfb93862321862f6d6d7a730d7db6bf020c52b
ShivMpluse <shivkumar.mpluse@gmail.com>
2024-12-04 13:55:19 +0530

    sefwf

--- Project-4/app/src/Components/Card.jsx
+++ Project-4/app/src/Components/Card.jsx
@@ -11,7 +11,7 @@ const Card = ({ data  }) => {
       <div className=" container card grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 flex-wrap gap-4 ">
         {
           data?.map(({name, image, text, price}) => (
-            <FoodCard key={name} className='border max-h-[150px] rounded-xl flex py-4 px-2 backdrop-blur-[8px] bg-white/5 gap-4'>
+            <DataCard key={name} className='border max-h-[150px] rounded-xl flex py-4 px-2 backdrop-blur-[8px] bg-white/5 gap-4'>
               <div className="img w-28 h-28">
                 <img src={BASE_URL + image} alt="logo" className='w-full h-full object-cover'/>
               </div>
@@ -20,11 +20,11 @@ const Card = ({ data  }) => {
                   <h3 className="font-semibold">{name}</h3>
                   <p className="text-sm">{text}</p>
                 </div>
-                <div className="btn flex justify-end sm:py-3 lg:py-7">
+                <div className="btn flex justify-end sm:py-3 lg:py-4">
                   <Button text={"$"+price.toFixed(2)} className='' />
                 </div>
               </div>
-            </FoodCard>
+            </DataCard> 
           ))
         }
       </div>
@@ -34,4 +34,4 @@ const Card = ({ data  }) => {
 
 export default Card
 
-const FoodCard = styled.div``;
+const DataCard = styled.div``;
